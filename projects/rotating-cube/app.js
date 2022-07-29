fb754a3eede0f778ca53170e3d8a0972fb8ffc98
Fabrizio Pace <pacefabriziowork@gmail.com>
2022-07-29 12:12:45 +0200

    Test

--- projects/rotating-cube/app.js
+++ projects/rotating-cube/app.js
@@ -7,8 +7,8 @@ class App {
         document.body.appendChild(container);
 
         // CAMERA
-        //this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
-        //this.camera.position.set(0, 0, 4);
+        this.camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
+        this.camera.position.set(0, 0, 4);
 
         // SCENE
         this.scene = new THREE.Scene();
