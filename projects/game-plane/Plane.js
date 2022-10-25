57d6e0f6dd1eb79c5d940b89e2bfbe8c24e4ffe4
Fabrizio Pace <pacefabriziowork@gmail.com>
2022-10-25 12:12:45 +0200

    Plane game

--- projects/game-plane/Plane.js
+++ projects/game-plane/Plane.js
@@ -16,10 +16,6 @@ class Plane {
         return this.tmpPos;
     }
 
-    set visible(mode) {
-        this.plane.visible = mode;
-    }
-
     load() {
         const loader = new GLTFLoader().setPath(`${this.assetsPath}plane/`);
         this.ready = false;
@@ -27,19 +23,24 @@ class Plane {
         // Load a glTF resource
         loader.load(
             // resource URL
-            'newplane.glb',
+            'microplane.glb',
             // called when the resource is loaded
             gltf => {
+
                 this.scene.add(gltf.scene);
                 this.plane = gltf.scene;
                 this.velocity = new Vector3(0, 0, 0.1);
-                this.propellor = this.plane.getObjectByName('propellor');
+
+                this.propeller = this.plane.getObjectByName("propeller");
 
                 this.ready = true;
+
             },
             // called while loading is progressing
             xhr => {
+
                 this.loadingBar.update('plane', xhr.loaded, xhr.total);
+
             },
             // called when loading has errors
             err => {
@@ -50,13 +51,28 @@ class Plane {
         );
     }
 
+    reset() {
+        this.plane.position.set(0, 0, 0);
+        this.velocity.set(0, 0, 0.1);
+    }
+
     update(time) {
-        if (this.propellor !== undefined) {
-            this.propellor.rotateZ(1);
+        if (this.propeller !== undefined) this.propeller.rotateZ(1);
+
+        if (this.game.active) {
+            if (!this.game.spaceKey) {
+                this.velocity.y -= 0.001;
+            } else {
+                this.velocity.y += 0.001;
+            }
+            this.plane.rotation.set(0, 0, Math.sin(time * 3) * 0.2, 'XYZ');
+            this.plane.translateZ(this.velocity.z);
+            this.plane.translateY(this.velocity.y);
+        } else {
+            this.plane.rotation.set(0, 0, Math.sin(time * 3) * 0.2, 'XYZ');
+            this.plane.position.y = Math.cos(time) * 1.5;
         }
 
-        this.plane.rotation.set(0, 0, Math.sin(time * 3) * 0.2, 'XYZ');
-        this.plane.position.y = Math.cos(time) * 1.5;
     }
 }
 
